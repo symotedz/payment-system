092ba01994a390bd10cb76d5c8ec5972b1fac921
Simon Kamau <simonkamau690@gmail.com>
2023-12-14 17:19:32 +0300

    template layout

--- mpesa/mpesa_payment_view.py
+++ mpesa/mpesa_payment_view.py
@@ -1,19 +1,6 @@
 from django import forms
 from django.core.validators import RegexValidator
 
-class ExpressNumberForm(forms.ModelForm):
-    phone = forms.CharField(
-        max_length=12,
-        min_length=10,
-        widget = forms.TextInput(attrs={'name': 'phone'}),
-        label = "Phone Number",
-        initial = 254,
-        help_text = "your mpesa registered number",
-    )
-    amount = forms.IntegerField(widget=forms.TextInput(attrs={'name' : 'amount'}), label='Amount', initial=1)
-    
-
-
 class MpesaPaymentForm(forms.Form):
     phone_number = forms.CharField(
         max_length=12,
