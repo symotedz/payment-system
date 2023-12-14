790469b09d5291496751966a3e9e88e54d189f17
Simon Kamau <simonkamau690@gmail.com>
2023-12-14 17:40:34 +0300

    collected uni test

--- mpesa/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from mpesa.transactions import mpesa_express
-# Create your tests here.
-class MpesaExpressTest(TestCase):
-    def test_mpesa_express(self):
-        self.assertEqual(mpesa_express(254114241129,1,"payment"),"success")
-
-
